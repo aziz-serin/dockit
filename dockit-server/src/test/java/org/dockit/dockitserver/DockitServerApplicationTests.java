8d7dd5fe4ff2c5de6f64c4389885178ec75faa0f
Aziz Serin <aziz.serin@student.manchester.ac.uk>
2023-10-25 12:53:35 +0100

    Init server project

---
+++ dockit-server/src/test/java/org/dockit/dockitserver/DockitServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dockit.dockitserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockitServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
