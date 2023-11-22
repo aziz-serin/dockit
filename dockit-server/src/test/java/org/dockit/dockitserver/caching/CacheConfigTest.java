1d2a0b64b5d87492cf20e60ebaf2f3218f352ea0
Aziz Serin <aziz.serin@student.manchester.ac.uk>
2023-11-22 15:48:35 +0000

    Write CacheConfigTest and AccessTokenServiceTest

---
+++ dockit-server/src/test/java/org/dockit/dockitserver/caching/CacheConfigTest.java
@@ -0,0 +1,45 @@
+package org.dockit.dockitserver.caching;
+
+import org.dockit.dockitserver.DockitServerApplication;
+import org.dockit.dockitserver.config.ConfigContainer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@DirtiesContext
+@ActiveProfiles("test")
+@SpringBootTest(classes = DockitServerApplication.class)
+@ExtendWith(SpringExtension.class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class CacheConfigTest {
+
+    @Autowired
+    private CacheConfig cacheConfig;
+    @Autowired
+    private ConfigContainer configContainer;
+
+    @Test
+    public void cachesAreInitialisedProperly() {
+        CacheManager cacheManager = cacheConfig.cacheManager(configContainer);
+        assertTrue(cacheManager instanceof CaffeineCacheManager);
+        assertThat(cacheManager.getCacheNames()).hasSize(4);
+        assertThat(cacheManager.getCacheNames()).containsAll(List.of(
+                CacheNames.AUDIT,
+                CacheNames.ACCESS_TOKEN,
+                CacheNames.AGENT,
+                CacheNames.ADMIN
+        ));
+    }
+}
--- dockit-server/src/test/java/org/dockit/dockitserver/services/AccessTokenServiceTest.java
+++ dockit-server/src/test/java/org/dockit/dockitserver/services/AccessTokenServiceTest.java
@@ -1,21 +1,26 @@
 package org.dockit.dockitserver.services;
 
 import org.dockit.dockitserver.DockitServerApplication;
+import org.dockit.dockitserver.caching.CacheNames;
 import org.dockit.dockitserver.entities.AccessToken;
 import org.dockit.dockitserver.services.templates.AccessTokenService;
 import org.dockit.dockitserver.entities.utils.EntityCreator;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cache.Cache;
+import org.springframework.cache.CacheManager;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Objects;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -26,8 +31,13 @@ import static org.assertj.core.api.Assertions.assertThat;
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class AccessTokenServiceTest {
 
+    private static final String CACHE_NAME = CacheNames.ACCESS_TOKEN;
+
     @Autowired
     private AccessTokenService accessTokenService;
+
+    @Autowired
+    private CacheManager cacheManager;
 
     private AccessToken accessToken1;
     private AccessToken accessToken2;
@@ -45,6 +55,11 @@ public class AccessTokenServiceTest {
 
         accessToken3 = EntityCreator.createAccessToken("token3", LocalDateTime.now().plusDays(1)).get();
         accessTokenService.save(accessToken3);
+    }
+
+    @AfterEach
+    public void clearCache() {
+        Objects.requireNonNull(cacheManager.getCache(CACHE_NAME)).clear();
     }
 
     @Test
@@ -82,6 +97,67 @@ public class AccessTokenServiceTest {
                 assertThat(token.getExpiryDate()).isAfter(LocalDateTime.now()));
 
         // Undo the effects of this test
+        accessTokenService.save(accessToken2);
+    }
+
+    @Test
+    public void saveCachesTheResultOfTheOperation() {
+        Cache cache = cacheManager.getCache(CACHE_NAME);
+        AccessToken tempToken = EntityCreator.createAccessToken("token", LocalDateTime.now().plusDays(1)).get();
+        accessTokenService.save(tempToken);
+
+        assertThat(cache).isNotNull();
+        AccessToken cachedToken = (AccessToken) Objects.requireNonNull(cache.get(tempToken.getId())).get();
+
+        assertThat(cachedToken).isNotNull();
+        assertThat(cachedToken.getId()).isEqualTo(tempToken.getId());
+
+        //Undo the effects of this test
+        accessTokenService.deleteById(tempToken.getId());
+    }
+
+    @Test
+    public void deleteByIdEvictsOnlyDeletedItemFromCache() {
+        Cache cache = cacheManager.getCache(CACHE_NAME);
+
+        AccessToken tempToken1 = EntityCreator.createAccessToken("token", LocalDateTime.now().plusDays(1)).get();
+        AccessToken tempToken2 = EntityCreator.createAccessToken("token", LocalDateTime.now().plusDays(1)).get();
+        accessTokenService.save(tempToken1);
+        accessTokenService.save(tempToken2);
+
+        assertThat(cache).isNotNull();
+
+        accessTokenService.deleteById(tempToken1.getId());
+
+        Object cachedToken1 = cache.get(tempToken1.getId());
+        assertThat(cachedToken1).isNull();
+
+        AccessToken cachedToken2 = (AccessToken) Objects.requireNonNull(cache.get(tempToken2.getId())).get();
+
+        assertThat(cachedToken2).isNotNull();
+        assertThat(cachedToken2.getId()).isEqualTo(tempToken2.getId());
+
+        //Undo the effects of this test
+        accessTokenService.deleteById(tempToken2.getId());
+    }
+
+    @Test
+    public void deleteExpiredEvictsAllCache() {
+        Cache cache = cacheManager.getCache(CACHE_NAME);
+
+        AccessToken tempToken = EntityCreator.createAccessToken("token", LocalDateTime.now().plusDays(1)).get();
+        accessTokenService.save(tempToken);
+
+        assertThat(cache).isNotNull();
+
+        accessTokenService.deleteExpired();
+
+        Object cachedToken = cache.get(tempToken.getId());
+        assertThat(cachedToken).isNull();
+
+
+        //Undo the effects of this test
+        accessTokenService.deleteById(tempToken.getId());
         accessTokenService.save(accessToken2);
     }
 }
