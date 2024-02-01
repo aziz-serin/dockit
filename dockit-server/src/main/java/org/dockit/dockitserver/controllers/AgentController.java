6e569a307cccaa8962b75bc2954f54dbf4833ecf
Aziz Serin <aziz.serin@student.manchester.ac.uk>
2024-02-01 22:39:26 +0000

    Create agent Controller

---
+++ dockit-server/src/main/java/org/dockit/dockitserver/controllers/AgentController.java
@@ -0,0 +1,138 @@
+package org.dockit.dockitserver.controllers;
+
+import org.dockit.dockitserver.controllers.utils.ParameterValidator;
+import org.dockit.dockitserver.entities.APIKey;
+import org.dockit.dockitserver.entities.Agent;
+import org.dockit.dockitserver.entities.utils.EntityCreator;
+import org.dockit.dockitserver.security.key.KeyHandler;
+import org.dockit.dockitserver.security.keystore.KeyStoreHandler;
+import org.dockit.dockitserver.services.templates.APIKeyService;
+import org.dockit.dockitserver.services.templates.AgentService;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.lang.NonNull;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Key;
+import java.time.LocalDateTime;
+import java.util.Base64;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+@RestController
+@RequestMapping(path = "/api/agent", produces = {MediaType.APPLICATION_JSON_VALUE})
+public class AgentController {
+    private final AgentService agentService;
+    private final APIKeyService apiKeyService;
+    private final KeyHandler keyHandler;
+    private final KeyStoreHandler keyStoreHandler;
+
+    public AgentController(AgentService agentService, APIKeyService apiKeyService, KeyHandler keyHandler,
+                           KeyStoreHandler keyStoreHandler) {
+        this.agentService = agentService;
+        this.apiKeyService = apiKeyService;
+        this.keyHandler = keyHandler;
+        this.keyStoreHandler = keyStoreHandler;
+    }
+
+    @GetMapping
+    @PreAuthorize("hasAnyAuthority('SUPER', 'EDITOR', 'VIEWER')")
+    public ResponseEntity<?> getAgent(@RequestParam(name = "id") long id) {
+        Optional<Agent> agent = agentService.findById(id);
+        if (agent.isEmpty()) {
+            return ResponseEntity.badRequest().body("Invalid request!");
+        } else {
+            return ResponseEntity.ok().body(agent.get());
+        }
+    }
+
+    @GetMapping("/all")
+    @PreAuthorize("hasAnyAuthority('SUPER', 'EDITOR', 'VIEWER')")
+    public ResponseEntity<?> getAgents() {
+        List<Agent> agents = agentService.findAll();
+        return ResponseEntity.ok(agents);
+    }
+
+    @GetMapping("/creationDate")
+    @PreAuthorize("hasAnyAuthority('SUPER', 'EDITOR', 'VIEWER')")
+    public ResponseEntity<?> getAgentsByCreationDate(@RequestParam(name = "isAscending") boolean isAscending) {
+        if (isAscending) {
+            return ResponseEntity.ok().body(agentService.findAllSortedByCreationDateAscending());
+        } else {
+            return ResponseEntity.ok().body(agentService.findAllSortedByCreationDateDescending());
+        }
+    }
+
+    @GetMapping("/active")
+    @PreAuthorize("hasAnyAuthority('SUPER', 'EDITOR', 'VIEWER')")
+    public ResponseEntity<?> getAgentsLastActiveInGivenMinutes(@RequestParam(name = "time") int minutes) {
+        return ResponseEntity.ok().body(agentService.findAllRecentlyActiveInGivenMinutes(minutes));
+    }
+
+    @PostMapping
+    @PreAuthorize("hasAnyAuthority('SUPER', 'EDITOR')")
+    public ResponseEntity<?> creteAgent(@RequestBody @NonNull Map<String, ?> body) {
+        String agentName = (String) body.get("agentName");
+        String password = (String) body.get("password");
+        if (!ParameterValidator.valid(agentName, password)) {
+            return ResponseEntity.badRequest().body("Invalid request!");
+        }
+        LocalDateTime creationTime = LocalDateTime.now();
+        LocalDateTime lastActiveTime = LocalDateTime.now();
+        Optional<Agent> agent = EntityCreator.createAgent(agentName, password, creationTime, lastActiveTime);
+
+        if (agent.isEmpty()) {
+            return ResponseEntity.badRequest().body("Invalid request!");
+        }
+        agentService.save(agent.get());
+        // Generate secret key for the agent and store it
+        Key key = keyHandler.generateKeyForAgentAndSave(agent.get().getId().toString(), password);
+
+        return ResponseEntity.ok().body(Map.of(
+                        "id", agent.get().getId(),
+                        "key", Base64.getEncoder().encodeToString(key.getEncoded()))
+        );
+    }
+
+    @DeleteMapping
+    @PreAuthorize("hasAnyAuthority('SUPER', 'EDITOR')")
+    public ResponseEntity<?> delete(@RequestParam(name = "id") long id) {
+        // Delete agent and any associated apikey with it, as well as its secret key
+        Optional<Agent> agent = agentService.findById(id);
+        if (agent.isEmpty()) {
+            return ResponseEntity.badRequest().body("Invalid request!");
+        }
+        List<Long> keyIds = apiKeyService.findAllWithSameAgent(agent.get())
+                .stream()
+                .map(APIKey::getId)
+                .toList();
+        apiKeyService.deleteAllById(keyIds);
+        agentService.deleteById(id);
+        keyStoreHandler.deleteKey(String.valueOf(id));
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping
+    @PreAuthorize("hasAnyAuthority('SUPER', 'EDITOR')")
+    public ResponseEntity<?> updateAgentName(@RequestParam(name = "id") long id,
+                                                    @RequestBody @NonNull Map<String, ?> body) {
+        String name = (String) body.get("agentName");
+        if (!ParameterValidator.valid(name)) {
+            return ResponseEntity.badRequest().body("Invalid request!");
+        }
+        Optional<Agent> agent = agentService.updateAgentName(id, name);
+        if (agent.isEmpty()) {
+            return ResponseEntity.badRequest().body("Invalid request!");
+        }
+        return ResponseEntity.ok().build();
+    }
+}
---
+++ dockit-server/src/test/java/org/dockit/dockitserver/controllers/AgentControllerTest.java
@@ -0,0 +1,285 @@
+package org.dockit.dockitserver.controllers;
+
+import org.dockit.dockitserver.DockitServerApplication;
+import org.dockit.dockitserver.authentication.issuer.APIKeyIssuer;
+import org.dockit.dockitserver.entities.APIKey;
+import org.dockit.dockitserver.entities.Admin;
+import org.dockit.dockitserver.entities.Agent;
+import org.dockit.dockitserver.entities.utils.EntityCreator;
+import org.dockit.dockitserver.security.keystore.KeyStoreHandler;
+import org.dockit.dockitserver.services.templates.APIKeyService;
+import org.dockit.dockitserver.services.templates.AdminService;
+import org.dockit.dockitserver.services.templates.AgentService;
+import org.dockit.dockitserver.testUtils.TokenObtain;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.web.reactive.function.BodyInserters;
+
+import java.security.Key;
+import java.time.LocalDateTime;
+import java.util.Base64;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicLong;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@DirtiesContext
+@ActiveProfiles("test")
+@SpringBootTest(classes = DockitServerApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ExtendWith(SpringExtension.class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class AgentControllerTest {
+
+    @LocalServerPort
+    int port;
+
+    @Autowired
+    AgentService agentService;
+    @Autowired
+    AdminService adminService;
+    @Autowired
+    APIKeyService apiKeyService;
+    @Autowired
+    KeyStoreHandler keyStoreHandler;
+    @Autowired
+    APIKeyIssuer apiKeyIssuer;
+
+    static final String AGENT_NAME = "agent";
+    static final String AGENT_PASSWORD = "password";
+    static final String ADMIN_USERNAME = "admin";
+    static final String ADMIN_PASSWORD = "password";
+    static final String VIEWER_ADMIN_USERNAME = "viewer";
+
+    WebTestClient client;
+    Agent agent;
+
+    @BeforeAll
+    public void setup() {
+        agent = EntityCreator.createAgent(AGENT_NAME, AGENT_PASSWORD, LocalDateTime.now(), LocalDateTime.now()).get();
+        agentService.save(agent);
+
+        Admin admin = EntityCreator.createAdmin(ADMIN_USERNAME, ADMIN_PASSWORD, Admin.Role.SUPER).get();
+        adminService.save(admin);
+
+        Admin viewerAdmin = EntityCreator.createAdmin(VIEWER_ADMIN_USERNAME, ADMIN_PASSWORD, Admin.Role.VIEWER).get();
+        adminService.save(viewerAdmin);
+
+        client = WebTestClient.bindToServer().baseUrl("http://localhost:" + port).build();
+    }
+
+    @Test
+    public void getAgentReturnsAgent() {
+        String jwt = TokenObtain.getJwt(ADMIN_USERNAME, ADMIN_PASSWORD, client);
+
+        client.get().uri("/api/agent?id=" + agent.getId())
+                .header("Authorization", jwt)
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(Map.class)
+                .consumeWith(res ->
+                        assertThat(res.getResponseBody().get("agentName")).isEqualTo(agent.getAgentName()));
+    }
+
+    @Test
+    public void getAllAgentsReturnsAgents() {
+        String jwt = TokenObtain.getJwt(ADMIN_USERNAME, ADMIN_PASSWORD, client);
+
+        client.get().uri("/api/agent/all")
+                .header("Authorization", jwt)
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(List.class)
+                .consumeWith(res ->
+                        assertThat(res.getResponseBody().size()).isEqualTo(agentService.findAll().size()));
+    }
+
+    @Test
+    public void getAllAgentsByCreationDateReturnsAgents() {
+        String jwt = TokenObtain.getJwt(ADMIN_USERNAME, ADMIN_PASSWORD, client);
+
+        client.get().uri("/api/agent/creationDate?isAscending=true")
+                .header("Authorization", jwt)
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(List.class)
+                .consumeWith(res ->
+                        assertThat(res.getResponseBody().size()).isEqualTo(agentService.findAll().size()));
+    }
+
+    @Test
+    public void getAllAgentsLastActiveInGivenMinutesReturnsAgents() {
+        String jwt = TokenObtain.getJwt(ADMIN_USERNAME, ADMIN_PASSWORD, client);
+
+        client.get().uri("/api/agent/active?time=" + 0)
+                .header("Authorization", jwt)
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(List.class)
+                .consumeWith(res ->
+                        assertThat(res.getResponseBody().size()).isEqualTo(0));
+    }
+
+    @Test
+    public void createAgentFailsGivenInsufficientPermissions() {
+        String jwt = TokenObtain.getJwt(VIEWER_ADMIN_USERNAME, ADMIN_PASSWORD, client);
+
+        client.post().uri("/api/agent")
+                .header("Authorization", jwt)
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().is4xxClientError();
+    }
+
+    @Test
+    public void createAgentFailsGivenInvalidBody() {
+        String jwt = TokenObtain.getJwt(ADMIN_USERNAME, ADMIN_PASSWORD, client);
+        Map<String, Object> json = Map.of(
+                "agentNme", AGENT_NAME,
+                "password", AGENT_PASSWORD
+        );
+
+        client.post().uri("/api/agent")
+                .header("Authorization", jwt)
+                .accept(MediaType.APPLICATION_JSON)
+                .body(BodyInserters.fromValue(json))
+                .exchange()
+                .expectStatus().is4xxClientError();
+    }
+
+    @Test
+    public void createAgentCreatesAgent() {
+        String jwt = TokenObtain.getJwt(ADMIN_USERNAME, ADMIN_PASSWORD, client);
+        Map<String, Object> json = Map.of(
+                "agentName", AGENT_NAME,
+                "password", AGENT_PASSWORD
+        );
+
+        client.post().uri("/api/agent")
+                .header("Authorization", jwt)
+                .accept(MediaType.APPLICATION_JSON)
+                .body(BodyInserters.fromValue(json))
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(Map.class)
+                .consumeWith(res -> {
+                    Map<String, Object> body = res.getResponseBody();
+                    long id = Long.parseLong(String.valueOf(Objects.requireNonNull(body).get("id")));
+                    String key = (String) Objects.requireNonNull(body).get("key");
+                    assertTrue(keyStoreHandler.keyExists(String.valueOf(id)));
+                    Optional<Key> optionalKey = keyStoreHandler.getKey(String.valueOf(id)
+                            , AGENT_PASSWORD.toCharArray());
+                    assertThat(optionalKey).isPresent();
+                    assertThat(Base64.getEncoder().encodeToString(optionalKey.get().getEncoded())).isEqualTo(key);
+                });
+    }
+
+    @Test
+    public void deleteFailsGivenInsufficientPermissions() {
+        String jwt = TokenObtain.getJwt(VIEWER_ADMIN_USERNAME, ADMIN_PASSWORD, client);
+
+        client.delete().uri("/api/agent?id=999")
+                .header("Authorization", jwt)
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isEqualTo(HttpStatusCode.valueOf(401));
+    }
+
+    @Test
+    public void deleteSucceeds() {
+        String jwt = TokenObtain.getJwt(ADMIN_USERNAME, ADMIN_PASSWORD, client);
+
+        // Create an agent with post request to generate the API key for it
+        Map<String, Object> json = Map.of(
+                "agentName", AGENT_NAME,
+                "password", AGENT_PASSWORD
+        );
+        AtomicLong id = new AtomicLong(0L);
+
+        client.post().uri("/api/agent")
+                .header("Authorization", jwt)
+                .accept(MediaType.APPLICATION_JSON)
+                .body(BodyInserters.fromValue(json))
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(Map.class)
+                .consumeWith(res -> {
+                            Map<String, Object> body = res.getResponseBody();
+                            id.set(Long.parseLong(String.valueOf(Objects.requireNonNull(body).get("id"))));
+                        });
+
+        // Create api key to observe it getting deleted
+        APIKey apiKey = apiKeyIssuer.issueKey(ADMIN_USERNAME, ADMIN_PASSWORD, id.get()).get();
+        apiKeyService.save(apiKey);
+
+        client.delete().uri("/api/agent?id=" + id.get())
+                .header("Authorization", jwt)
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk();
+
+        // Check that apiKey and secretKey is deleted
+        assertThat(apiKeyService.findAll()).hasSize(0);
+        assertFalse(keyStoreHandler.keyExists(String.valueOf(id.get())));
+    }
+
+    @Test
+    public void updateAgentNameFailsGivenInsufficientPermission() {
+        String jwt = TokenObtain.getJwt(VIEWER_ADMIN_USERNAME, ADMIN_PASSWORD, client);
+
+        client.put().uri("/api/agent?id=999")
+                .header("Authorization", jwt)
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().is4xxClientError();
+    }
+
+    @Test
+    public void updateAgentNameFailsGivenInvalidBody() {
+        String jwt = TokenObtain.getJwt(ADMIN_USERNAME, ADMIN_PASSWORD, client);
+        Map<String, Object> json = Map.of(
+                "agentNme", AGENT_NAME
+        );
+
+        client.put().uri("/api/agent?id=999")
+                .header("Authorization", jwt)
+                .body(BodyInserters.fromValue(json))
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().is4xxClientError();
+    }
+
+    @Test
+    public void updateAgentNameSucceeds() {
+        String jwt = TokenObtain.getJwt(ADMIN_USERNAME, ADMIN_PASSWORD, client);
+        Map<String, Object> json = Map.of(
+                "agentName", AGENT_NAME
+        );
+
+        client.put().uri("/api/agent?id=" + agent.getId())
+                .header("Authorization", jwt)
+                .body(BodyInserters.fromValue(json))
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk();
+    }
+}
