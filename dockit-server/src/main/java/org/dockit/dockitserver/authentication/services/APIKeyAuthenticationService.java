663c75d466c1228df2fa18706a66b4666fbbc173
Aziz Serin <aziz.serin@student.manchester.ac.uk>
2024-02-07 21:24:24 +0000

    Update last active time of the agent once it is authenticated

--- dockit-server/src/main/java/org/dockit/dockitserver/authentication/services/APIKeyAuthenticationService.java
+++ dockit-server/src/main/java/org/dockit/dockitserver/authentication/services/APIKeyAuthenticationService.java
@@ -3,7 +3,9 @@ package org.dockit.dockitserver.authentication.services;
 import jakarta.servlet.http.HttpServletRequest;
 import org.dockit.dockitserver.authentication.authentication.APIKeyAuthentication;
 import org.dockit.dockitserver.entities.APIKey;
+import org.dockit.dockitserver.entities.Agent;
 import org.dockit.dockitserver.services.templates.APIKeyService;
+import org.dockit.dockitserver.services.templates.AgentService;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.authority.AuthorityUtils;
@@ -11,6 +13,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 /**
@@ -21,16 +24,20 @@ import java.util.List;
 public class APIKeyAuthenticationService {
     private static final String AUTH_TOKEN_HEADER_NAME = "X-API-KEY";
     private static APIKeyService apiKeyService;
+    private static AgentService agentService;
 
     /**
      * @param apiKeyService {@link APIKeyService} object to be injected
+     * @param agentService {@link AgentService} object to be injected
      */
-    public APIKeyAuthenticationService(APIKeyService apiKeyService) {
+    public APIKeyAuthenticationService(APIKeyService apiKeyService, AgentService agentService) {
         APIKeyAuthenticationService.apiKeyService = apiKeyService;
+        APIKeyAuthenticationService.agentService = agentService;
     }
 
     /**
-     * Extracts the apikey from the request headers, and validates it
+     * Extracts the apikey from the request headers, validates it, and updates last seen time of
+     * agent if the provided apikey is valid
      *
      * @param request Incoming http request
      * @return {@link APIKeyAuthentication} if successfully authenticated
@@ -41,6 +48,8 @@ public class APIKeyAuthenticationService {
         if (apiKey == null || !validAPIKey(apiKey)) {
             throw new BadCredentialsException("Invalid API Key!");
         }
+        // Update the last active time before authentication is created and after an api key is validated.
+        updateLastActiveTimeForAgent(apiKey);
         return new APIKeyAuthentication(apiKey, AuthorityUtils.NO_AUTHORITIES);
     }
 
@@ -53,5 +62,19 @@ public class APIKeyAuthenticationService {
                     PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
                     return passwordEncoder.matches(apiKey, key);
                 });
+    }
+
+    private static void updateLastActiveTimeForAgent(String apiKey) {
+        List<APIKey> keys = apiKeyService.findAll();
+        // This method will be called after validating the key, so just getting the agent is fine
+        Agent agent = keys.stream()
+                .filter(key -> {
+                    PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+                    return passwordEncoder.matches(apiKey, key.getToken());
+                }).map(APIKey::getAgent)
+                .toList()
+                .get(0);
+        // Update the last active time of the agent with now
+        agentService.updateLastActiveTime(agent.getId(), LocalDateTime.now());
     }
 }
