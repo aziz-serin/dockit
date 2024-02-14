4f71510f57c81ddebbf74d904b4c65683b10fd01
Aziz Serin <aziz.serin@student.manchester.ac.uk>
2024-02-14 16:27:26 +0000

    Update delimiter

--- dockit-agent/src/main/java/org/dockit/dockitagent/collector/utils/InformationBuilderHelper.java
+++ dockit-agent/src/main/java/org/dockit/dockitagent/collector/utils/InformationBuilderHelper.java
@@ -2,12 +2,23 @@ package org.dockit.dockitagent.collector.utils;
 
 import java.util.List;
 
+/**
+ * Helper class to build string from a list of data
+ */
 public final class InformationBuilderHelper {
+    public static final String DELIMITER = "/////////////////////";
+
+    /**
+     * Build one large string with a delimiter from list of data
+     *
+     * @param givenData {@link List} of data items
+     * @return string representation of produced data
+     */
     public static String build(List<String> givenData) {
         StringBuilder stringBuilder = new StringBuilder();
         for (String data : givenData) {
             stringBuilder.append(data);
-            stringBuilder.append(" ");
+            stringBuilder.append(DELIMITER);
         }
         return stringBuilder.toString();
     }
