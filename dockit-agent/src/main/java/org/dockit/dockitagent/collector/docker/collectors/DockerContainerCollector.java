4f068e3c836f7e37d921e12e815fce2e4093e1a2
Aziz Serin <aziz.serin@student.manchester.ac.uk>
2024-02-17 17:04:25 +0000

    Update returned string

--- dockit-agent/src/main/java/org/dockit/dockitagent/collector/docker/collectors/DockerContainerCollector.java
+++ dockit-agent/src/main/java/org/dockit/dockitagent/collector/docker/collectors/DockerContainerCollector.java
@@ -1,10 +1,15 @@
 package org.dockit.dockitagent.collector.docker.collectors;
 
+import com.google.gson.Gson;
 import com.google.inject.Inject;
 import org.dockit.dockitagent.collector.Collector;
+import org.dockit.dockitagent.collector.utils.InformationBuilderHelper;
 import org.dockit.dockitagent.connection.ConnectionManager;
 import org.dockit.dockitagent.connection.DockerConnectionManager;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 public class DockerContainerCollector implements Collector {
@@ -20,6 +25,17 @@ public class DockerContainerCollector implements Collector {
     @Override
     public String collect() {
         Optional<String> response = connectionManager.sendRequest(ENDPOINT);
-        return response.orElse(null);
+        if (response.isEmpty()) {
+            return null;
+        }
+        Gson gson = new Gson();
+        List<Map<String, String>> dataList = gson.fromJson(response.get(), List.class);
+        List<String> data = new ArrayList<>();
+
+        for (Map<String, String> dataItem : dataList) {
+            data.add(gson.toJson(dataItem));
+        }
+
+        return InformationBuilderHelper.build(data);
     }
 }
