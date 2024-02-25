1b7ae24fc71cc250fad5bea1acd5c0a9f41f3304
Aziz Serin <aziz.serin@student.manchester.ac.uk>
2024-02-25 14:19:44 +0000

    Add javadoc to routes

--- dockit-agent/src/main/java/org/dockit/dockitagent/rest/routes/CommandRoute.java
+++ dockit-agent/src/main/java/org/dockit/dockitagent/rest/routes/CommandRoute.java
@@ -12,15 +12,29 @@ import spark.Route;
 import java.util.Map;
 import java.util.Optional;
 
+/**
+ * Route for the /command endpoint
+ */
 public class CommandRoute implements Route {
 
     private final CommandTranslator commandTranslator;
 
+    /**
+     * @param commandTranslator {@link CommandTranslator} instance to be injected
+     */
     @Inject
     public CommandRoute(CommandTranslator commandTranslator) {
         this.commandTranslator = commandTranslator;
     }
 
+    /**
+     * Controller method to handle /command requests.
+     * Try to parse and execute the command, if not fail and return the appropriate error code.
+     *
+     * @param request Upcoming request
+     * @param response Upcoming response
+     * @return status code with the appropriate response body
+     */
     @Override
     public Object handle(Request request, Response response) {
         Optional<Command> command = commandTranslator.getCommand(request.body());
--- dockit-agent/src/main/java/org/dockit/dockitagent/rest/routes/PingRoute.java
+++ dockit-agent/src/main/java/org/dockit/dockitagent/rest/routes/PingRoute.java
@@ -4,9 +4,17 @@ import spark.Request;
 import spark.Response;
 import spark.Route;
 
+/**
+ * Route for the /ping endpoint
+ */
 public class PingRoute implements Route {
+
+
+    /**
+     * Dummy endpoint which indicates that the server is running
+     */
     @Override
-    public Object handle(Request request, Response response) throws Exception {
+    public Object handle(Request request, Response response) {
         response.status(200);
         return "Alive";
     }
