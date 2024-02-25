67776328330ee116601d6b89077270fe45bfb7f3
Aziz Serin <aziz.serin@student.manchester.ac.uk>
2024-02-25 14:23:09 +0000

    Add javadoc

--- dockit-agent/src/main/java/org/dockit/dockitagent/rest/RestModule.java
+++ dockit-agent/src/main/java/org/dockit/dockitagent/rest/RestModule.java
@@ -1,9 +1,14 @@
 package org.dockit.dockitagent.rest;
 
 import com.google.inject.AbstractModule;
+import org.dockit.dockitagent.rest.routes.CommandRoute;
 
+/**
+ * Guice bindings for the api-related classes
+ */
 public class RestModule extends AbstractModule {
     public void configure() {
         bind(RestServer.class).asEagerSingleton();
+        bind(CommandRoute.class);
     }
 }
--- dockit-agent/src/main/java/org/dockit/dockitagent/rest/RestServer.java
+++ dockit-agent/src/main/java/org/dockit/dockitagent/rest/RestServer.java
@@ -1,66 +1,46 @@
 package org.dockit.dockitagent.rest;
 
-import com.google.gson.Gson;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
-import org.dockit.dockitagent.command.Command;
-import org.dockit.dockitagent.command.CommandExecutor;
-import org.dockit.dockitagent.command.CommandTranslator;
-import spark.Request;
-import spark.Response;
-import spark.Route;
-
-import java.util.Map;
-import java.util.Optional;
+import org.dockit.dockitagent.rest.routes.CommandRoute;
+import org.dockit.dockitagent.rest.routes.PingRoute;
 
 import static spark.Spark.after;
 import static spark.Spark.post;
 import static spark.Spark.get;
 
+/**
+ * Server to be started to accept requests
+ */
 @Singleton
 public class RestServer {
 
-    private static CommandTranslator commandTranslator;
+    private final CommandRoute commandRoute;
+    private final PingRoute pingRoute;
 
+    /**
+     * Inject any route to the constructor and then create the appropriate endpoint in setupEndpoints() method
+     *
+     * @param commandRoute {@link CommandRoute} instance to be injected
+     * @param pingRoute {@link PingRoute} instance to be injected
+     */
     @Inject
-    public RestServer(CommandTranslator commandTranslator) {
-        RestServer.commandTranslator = commandTranslator;
+    public RestServer(CommandRoute commandRoute, PingRoute pingRoute) {
+        this.commandRoute = commandRoute;
+        this.pingRoute = pingRoute;
     }
 
+    /**
+     * Setup all the injected endpoints in this method
+     */
     public void setupEndpoints() {
-        post("/command", (request, response) -> {
-            Optional<Command> command = commandTranslator.getCommand(request.body());
-            Gson gson = new Gson();
-            if (command.isEmpty()) {
-                response.status(400);
-                return gson.toJson(Map.of(
-                        "message", "Invalid request!"
-                ));
-            }
-            CommandExecutor commandExecutor = new CommandExecutor();
-            if (commandExecutor.execute(command.get())) {
-                response.status(200);
-                return gson.toJson(Map.of(
-                        "message", "Successful"
-                ));
-            } else {
-                response.status(500);
-                return gson.toJson(Map.of(
-                        "message", "Could not execute the command!"
-                ));
-            }
-        });
+        post("/command", commandRoute);
 
         // Dummy endpoint to indicate the server is alive
-        get("/ping", RestServer.ping);
+        get("/ping", pingRoute);
 
         after((request, response) -> {
             response.type("application/json");
         });
     }
-
-    public static Route ping = (Request req, Response res) -> {
-        res.status(200);
-        return "Alive";
-    };
 }
